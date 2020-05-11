469b434f0297cafaf74caa545317228e5b4ad6f6
Shahid <59953507+Shahidmumtaz@users.noreply.github.com>
2020-05-11 16:31:44 -0700

    Add files via upload

--- db/scripts.sql
+++ db/scripts.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS unemployment;
 DROP TABLE IF EXISTS mortgage_interest;
 DROP TABLE IF EXISTS GDP_states;
 DROP TABLE IF EXISTS final_data;
+DROP TABLE IF EXISTS lat_lon;
+DROP TABLE IF EXISTS state_pop;
+DROP TABLE IF EXISTS state_pop_30m;
 
 CREATE TABLE unemployment(
     year INT,
@@ -42,4 +45,26 @@ CREATE TABLE final_data(
     pop_2010 INT,
     pop_2018 INT
 
+);
+
+CREATE TABLE lat_lon(
+    state VARCHAR (150),
+    latitude DECIMAL (10, 2),
+ 	longitude DECIMAL (10, 2)
+);
+
+CREATE TABLE state_pop_in_m(
+    state VARCHAR (150),
+ 	pop_1990_m INT,
+ 	pop_2000_m INT,
+ 	pop_2010_m INT,
+ 	pop_2018_m INT
+);
+
+CREATE TABLE state_pop_30m(
+    state VARCHAR (150),
+ 	pop_1990_m INT,
+ 	pop_2000_m INT,
+ 	pop_2010_m INT,
+ 	pop_2018_m INT
 );
